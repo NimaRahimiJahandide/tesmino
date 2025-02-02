4bf6a8242e8773faab0e6e9bdf5c8c3d7f76e2b8
Nima <nimarahimijahandide@gmail.com>
2025-02-03 00:33:59 +0330

    fix footer

--- src/components/common/FooterComponent.vue
+++ src/components/common/FooterComponent.vue
@@ -9,15 +9,20 @@ const enlargeImage = () => {
     imgScale.value = 1;
   }, 300);
 };
+
+const resetImageScale = () => {
+  imgScale.value = 1;
+};
 </script>
 
 <template>
-  <div class="bg-[#32cd95] rounded-t-[60px] relative overflow-hidden">
+  <div class="bg-[#32cd95] rounded-t-[60px] relative overflow-hidden" @mouseenter="enlargeImage"
+    @mouseleave="resetImageScale">
     <div class="h-[95px] flex items-end">
       <div
         class="bg-[#0f172a] font-Righteous text-[#768194] rounded-t-[60px] h-[90px] w-full flex gap-2 justify-center items-center">
         Give me FIVE
-        <img @click="enlargeImage"  :style="{ scale: imgScale }" src="/icons/give-hand.svg" alt="give-hand"
+        <img @click="enlargeImage" :style="{ scale: imgScale }" src="/icons/give-hand.svg" alt="give-hand"
           class="cursor-pointer transition-all duration-300">
       </div>
     </div>
